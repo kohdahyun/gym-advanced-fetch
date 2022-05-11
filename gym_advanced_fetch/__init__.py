2ee506169a51a074d7a605c75dd1e373c1ecdd25
KOHDAHYUN <kohdh20@yonsei.ac.kr>
2022-05-11 22:43:15 +0900

    add FetchReachWithObstacle-v0

--- gym_advanced_fetch/__init__.py
+++ gym_advanced_fetch/__init__.py
@@ -5,3 +5,9 @@ register(
         entry_point="gym_advanced_fetch.envs:FetchReachAndThrowEnv",
         max_episode_steps=50,
         )
+
+register(
+        id="FetchReachWithObstacle-v0",
+        entry_point="gym_advanced_fetch.envs:FetchReachWithObstacleEnv",
+        max_episode_steps=50,
+        )
--- gym_advanced_fetch/envs/__init__.py
+++ gym_advanced_fetch/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_advanced_fetch.envs.reach_and_throw import FetchReachAndThrowEnv
+from gym_advanced_fetch.envs.reach_with_obstacle import FetchReachWithObstacleEnv
